1aaaea57ddd9a741b5c3bc5c69227b883cd841b9
Salva Martí <salvadormartisolsona@gmail.com>
2019-07-08 20:27:53 +0200

    Initial Commit

---
+++ Merlin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Merlin
+//
+//  Created by Salvador Martí Solsona on 08/07/2019.
+//  Copyright © 2019 Salvador Martí Solsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
